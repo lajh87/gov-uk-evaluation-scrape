2bbf30b4491113295afe99c25b1815d9351e3285
lheley <lajh87@live.co.uk>
2023-06-18 10:18:33 +0100

    update keyword match (phew)

--- data-raw/4-keyword_match.R
+++ data-raw/4-keyword_match.R
@@ -1,14 +1,21 @@
-#' This file searches for keywords in documents and returns the document id and number of matches
-#' 
+#' This file loads the raw data, extracts the text and converts it to sentences.
+#' It then extracts keywords from the sentences if they exist.
+#' This script takes about 40 minutes to run.
+
+doc_links <- readr::read_csv("data-raw/20230614-gov-uk-evaluation-doc-links.csv") |>
+  dplyr::filter(stringr::str_detect(documents, "gov.uk")) |>
+  dplyr::pull(documents)
+
 keywords <- readLines("data-raw/keywords.txt")
+search_ <- keywords |> paste(collapse="|")
+
 files <- list.files("data-raw", pattern = "docs-")
 
 keyword_match <- purrr::map_df(cli::cli_progress_along(files), function(i){
   rds <- files[i]
-  content <- readRDS(file.path("data-raw", rds))
+  raw <- readRDS(file.path("data-raw", rds))
   
-  # Extract text from pdf and html documents
-  text <- purrr::map_df(content, ~{
+  text <- purrr::map_df(raw, ~{
     if(.x[[2]] == "application/pdf"){
       text <- pdftools::pdf_text(.x[[3]]) |> paste(collapse = "\n")
     } else{
@@ -19,39 +26,30 @@ keyword_match <- purrr::map_df(cli::cli_progress_along(files), function(i){
     dplyr::tibble(id = .x[[1]], content_type = .x[[2]], text)
   })
   
-  # Extract Trigrams, Bigrams and Word lookup tables
-  trigram <- text |>
-    tidytext::unnest_ngrams(trigram, text, 3)
-  bigram <- text |>
-    tidytext::unnest_ngrams(bigram, text, 2)
-  word <- text |>
-    tidytext::unnest_tokens(word, text)
+  sentences <- text |>
+    dplyr::filter(!is.na(text)) |>
+    tidytext::unnest_sentences("sentence", text, TRUE) 
   
-  # Match keyword to the lookup table and return location
-  keyword_match <- purrr::map_df(keywords, ~{
-    words <- (stringr::str_locate_all(.x, "\\s")[[1]] |> length())/2+1
-    loc <- NULL
-    if(words == 1){
-      loc <- word |> dplyr::filter(stringr::str_detect(.x, word)) |> dplyr::pull(id)
-    } else{
-      if(words == 2){
-        loc <- bigram |> dplyr::filter(stringr::str_detect(.x, bigram)) |> dplyr::pull(id)
-      } else{
-        loc <- trigram |> dplyr::filter(stringr::str_detect(.x, trigram)) |> dplyr::pull(id)
-      }
-    }
-    if(!is.null(loc)){
-      dplyr::tibble(source = rds, keyword = .x, doc.id = loc)
-    } else{
-      dplyr::tibble(source = rds, keyword = .x, doc.id = NA)
-    }
-  })
-  
-  keyword_match |>
-    dplyr::group_by(source, keyword, doc.id) |>
-    dplyr::count()
+  sentences |>
+    dplyr::mutate(keywords = stringr::str_extract_all(sentence, search_)) |>
+    tidyr::unnest("keywords") |> 
+    dplyr::group_by(id) |>
+    dplyr::summarise(paste(unique(keywords), collapse = ", ")) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(source = rds)
 })
 
-write.csv(keyword_match, "data-raw/keyword-match.csv", row.names = FALSE)
+keyword_match |>
+  dplyr::mutate(doc_id =  substr(source, 6, 10)) 
+
+keyword_match2 <- keyword_match |>
+  dplyr::mutate(doc_id =  as.numeric(substr(source, 6, 10)) + id - 1) |>
+  dplyr::left_join(
+    dplyr::tibble(documents = doc_links) |>
+      dplyr::mutate(doc_id = 1:dplyr::n()),
+    by = "doc_id"
+  ) |>
+  dplyr::rename(keywords = 2)
 
 
+write.csv(keyword_match2, "data-raw/20230614-keyword-match.csv", row.names = FALSE)
